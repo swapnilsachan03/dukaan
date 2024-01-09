18bbd4b7bb20a58565235cfa1faf6aaa521e65ac
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2024-01-10 00:28:05 +0530

    intial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,208 @@
-import Image from 'next/image'
+import { FaChevronDown, FaChevronRight, FaHouseChimney, FaMagnifyingGlass, FaQuestion, FaWallet } from "react-icons/fa6";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { RiArrowUpDownLine } from "react-icons/ri";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { GrAnnounce } from "react-icons/gr";
+import { GoHome, GoQuestion } from "react-icons/go";
+import { BiNotepad } from "react-icons/bi";
+import { TfiAnnouncement } from "react-icons/tfi";
+import { RiCaravanLine } from "react-icons/ri";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+import { VscGraph } from "react-icons/vsc";
+import { HiBanknotes } from "react-icons/hi2";
+import { TbDiscount2 } from "react-icons/tb";
+import { MdOutlinePeopleAlt } from "react-icons/md";
+import { IoColorPaletteOutline } from "react-icons/io5";
+import { IoFlashOutline } from "react-icons/io5";
+
+const navbarItems = [
+  { title: "Home", icon: GoHome },
+  { title: "Orders", icon: BiNotepad },
+  { title: "Products", icon: HiOutlineSquares2X2 },
+  { title: "Delivery", icon: RiCaravanLine },
+  { title: "Marketing", icon: TfiAnnouncement },
+  { title: "Analytics", icon: VscGraph },
+  { title: "Payouts", icon: HiBanknotes },
+  { title: "Discounts", icon: TbDiscount2 },
+  { title: "Audience", icon: MdOutlinePeopleAlt },
+  { title: "Appearance", icon: IoColorPaletteOutline },
+  { title: "Plugins", icon: IoFlashOutline },
+]
+
+const table = [
+  'Order ID', 'Status', 'Transaction ID', 'Refund Date'
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex flex-row'>
+      <div className='w-[224px] h-screen bg-[#1E2640] relative text-white'>
+        <div className='flex flex-col items-center gap-6 px-4 py-[10px]'>
+          <div className='w-[192px] h-[42px] flex gap-3 relative'>
+            <div className='w-[39px] h-[39px] rounded-md bg-white flex items-center justify-center'>
+              <p className='text-[6px] text-gray-500'> Nishyam </p>
+            </div>
+
+            <div className='flex flex-col gap-1 px-0.5'>
+              <p className='text-sm font-medium'> Nishyan </p>
+              <p className='text-xs text-gray-400 underline'> Visit store </p>
+            </div>
+
+            <div className='absolute self-center right-0'>
+              <FaChevronDown size={20} color='white' />
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            { navbarItems.map(item => (
+              <div className={`w-[208px] flex flex-row rounded py-2 px-4 gap-3 ${item.title === 'Payouts' && 'bg-white/10'}`}>
+                <item.icon size={20} />
+                <p className='text-sm font-medium'> {item.title} </p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className='w-[192px] h-[54px] py-[6px] px-[12px] bg-[#353C53] rounded absolute bottom-4 left-4 flex flex-row'>
+          <div className='w-[149px] h-[42px] flex flex-row gap-3 items-center justify-center'>
+            <div className='bg-white/10 flex items-center justify-center p-1.5 rounded w-9 h-9'>
+              <FaWallet color='white' />
+            </div>
+
+            <div className='flex flex-col gap-[2px] w-[101px] h[42px]'>
+              <p className='text-[12px] leading-4'> Available Credits </p>
+              <p className='font-medium text-base'> 222.0 </p>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className=' w-[1216px] h-screen'>
+        <nav className='h-16 py-3 px-8 border-b-[1px] flex flex-row items-center gap-4'>
+          <div className='w-[360px] h-[28px] flex flex-row items-center gap-4'>
+            <h5 className='font-medium text-xl'> Payouts </h5>
+
+            <div className='h-4 flex flex-row gap-1.5 text-black/30 items-center'>
+              <GoQuestion size={14} />
+              <p className='text-[12px]'> How it works? </p>
+            </div>
+          </div>
+
+          <div className='w-[400px] h-[40px] flex flex-row gap-2 py-[9px] px-4 items-center rounded-md bg-[#F2F2F2] text-black/50'>
+            <FaMagnifyingGlass size={14} />
+            <p className='text-[15px]'> Search features, tutorials, etc. </p>
+          </div>
+          
+          <div className='w-[360px] h-[40px] flex flex-row justify-end gap-3 text-[#4D4D4D]'>
+            <div className='rounded-full h-10 w-10 bg-[#F2F2F2] flex items-center justify-center'>
+              <GrAnnounce size={20} />
+            </div>
+
+            <div className='rounded-full h-10 w-10 bg-[#F2F2F2] flex items-center justify-center'>
+              <IoMdArrowDropdown size={25} />
+            </div>
+          </div>
+        </nav>
+
+        <div className='top-[96px] left-[256px] pt-8 pl-8'>
+          <div className='w-[1152px] h-[794px]'>
+            <div className='w-full h-[214px] flex flex-col gap-8'>
+              <div className='h-[36px] flex flex-row justify-between items-center'>
+                <p className='text-xl font-medium'> Overview </p>
+                <button className='w-[137px] h-[36px] flex flex-row gap-4 justify-center items-center rounded border-[1px]'>
+                  <p className='text-base'> This Month </p>
+                  <FaChevronDown size={16} />
+                </button>
+              </div>
+
+              <div className='flex flex-row h-[154px] gap-5'>
+                <div className='h-[154px] w-[370.67px] bg-[#146EB4] rounded-lg flex flex-col'>
+                  <div></div>
+                </div>
+
+                <div className='h-[118px] w-[370.67px] flex flex-col p-5 shadow-md rounded'>
+                  <div className='h-[78px] w-[330.67px] flex flex-col gap-4 items-stretch'>
+                    <div className='flex flex-row items-center gap-2 text-[#4D4D4D]'>
+                      <p> Amount Pending </p>
+                      <GoQuestion size={14} />
+                    </div>
+
+                    <div className='flex flex-row justify-between items-baseline'>
+                      <p className='text-3xl font-medium'> ₹92,312.20 </p>
+
+                      <div className='flex flex-row gap-1 items-center text-blue-500'>
+                        <p className='underline underline-offset-2 font-medium'> 13 Orders </p>
+                        <FaChevronRight size={14} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+                <div className='h-[118px] w-[370.67px] flex flex-col p-5 shadow-md rounded'>
+                  <div className='h-[78px] w-[330.67px] flex flex-col gap-4 items-stretch'>
+                    <div className='flex flex-row items-center gap-2 text-[#4D4D4D]'>
+                      <p> Amount Processed </p>
+                      <GoQuestion size={14} />
+                    </div>
+
+                    <div className='flex flex-row justify-between items-baseline'>
+                      <p className='text-3xl font-medium'> ₹23,92,312.19 </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='h-[548px] w-full flex flex-col gap-6'>
+                <div className='h-20 w-[252px] flex flex-col gap-6'>
+                  <p className='font-medium text-xl'> Transactions | This Month </p>
+
+                  <div className="flex flex-row h-8 gap-3">
+                    <div className='rounded-[40px] py-[6px] px-4 bg-[#146EB4] text-white'>
+                      <p className='text-sm font-medium'> Payouts (22) </p>
+                    </div>
+
+                    <div className='rounded-[40px] py-[6px] px-4 bg-[#E6E6E6] text-[808080]'>
+                      <p className='text-sm font-medium'> Refunds (2) </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='p-3 pb-2 rounded-lg h-[400px] shadow-md'>
+                  <div className='flex flex-col h-[92px] gap-3'>
+                    <div className='h-10 flex flex-row items-center justify-between'>
+                      <div className='w-[248px] h-full flex flex-row items-center gap-2 py-2.5 px-4 border-[1px] rounded-md text-black/60'>
+                        <FaMagnifyingGlass size={14} />
+                        <p className='text-sm'> Order ID or transaction ID </p>
+                      </div>
+
+                      <div className='h-8 flex flex-row items-center gap-3'>
+                        <button className='flex flex-row items-center gap-3 py-[5px] px-3 text-black/60 border-[1px] rounded-md'>
+                          <p className='text-base'> Sort </p>
+                          <RiArrowUpDownLine />
+                        </button>
+
+                        <button className='h-[36px] w-[36px] flex items-center justify-center text-black/60 border-[1px] rounded-md'>
+                          <MdOutlineFileDownload size={20} />
+                        </button>
+                      </div>
+                    </div>
+
+                    <div className='rounded-md flex flex-row items-center justify-between py-2.5 px-3 h-10 bg-[#F2F2F2]'>
+                      { table.map((item, index) => (
+                        <p className='w-[150.67px] text-sm font-medium' key={index}> {item} </p>
+                      ))}
+
+                      <p className='w-[150.67px] text-sm font-medium text-right'> Order Amount </p>
+                    </div>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
